375bc3975bf0553cc3c4b3993ee5edbf0c312e52
Your Name <you@example.com>
2020-10-26 18:33:36 +0100

    Animation added, projects page for users updated

--- FrontEnd/src/app/components/users/projects/list/list.component.ts
+++ FrontEnd/src/app/components/users/projects/list/list.component.ts
@@ -3,6 +3,7 @@ import { ProjectService } from 'src/app/services/project.service';
 import { Project } from 'src/app/Models/Project';
 import { take } from 'rxjs/operators';
 import { Task } from 'src/app/Models/Tasks';
+import { defineCustomElements } from '@teamhive/lottie-player/loader';
 
 @Component({
   selector: 'app-list',
@@ -11,19 +12,25 @@ import { Task } from 'src/app/Models/Tasks';
 })
 export class ListComponent implements OnInit {
   loading: boolean;
-  projects: Project[];
+  projects: Project[] = [];
   completedTasks: Task[][];
-  notAssignedTasks : Task[][];
+  notAssignedTasks: Task[][];
+
   constructor(private projectService: ProjectService) {}
 
   ngOnInit(): void {
+    defineCustomElements(window);
     this.projectService
       .getAllProjects()
       .pipe(take(1))
       .subscribe((projects: Project[]) => {
         this.projects = projects;
-        this.completedTasks = projects.map(x => x.tasks.filter((x:Task) => x.status == 3))
-        this.notAssignedTasks = projects.map(x => x.tasks.filter((x:Task) => x.userId == 0))
+        this.completedTasks = projects.map((x) =>
+          x.tasks.filter((x: Task) => x.status == 3)
+        );
+        this.notAssignedTasks = projects.map((x) =>
+          x.tasks.filter((x: Task) => x.userId == 0)
+        );
         this.loading = false;
       });
   }
