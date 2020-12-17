eb7201bf0d1012cb1c3b7a89d4748ca289c6a799
Kaxuo <50716622+Kaxuo@users.noreply.github.com>
2020-12-17 15:15:58 +0100

    Azure b2 c (#19)
    * test
    
    * Create .deployment
    
    * test again
    
    * production build
    
    * cleanup
    
    * trying
    
    * start
    
    * Tracking Online/offline users working
    
    * Pages added for Rooms and one on one conversations
    
    * Functionalities added for rooms
    
    * Group Chat added and working
    
    * group chat completed and working, small functionalities perfected
    
    * cleanup
    
    * Update appsettings.json
    
    * requested changed added
    
    * Update appsettings.json
    
    * OneOnOne Conversation Updated but not finished
    
    * OneOnOne working now
    
    * OneonOne working, non used import removed
    
    * wwrootremoved
    
    * bugfixes
    
    * conflicts resolved
    
    * Cleanup
    
    * Azure B2C working with application
    
    * AZureB2C now working
    
    * Bugfixes
    
    * Deployment
    
    * deploy
    
    * CleanUp
    
    * removed package
    
    * Working now
    
    * removed

--- BackEnd/Data/AppSettings.cs
+++ BackEnd/Data/AppSettings.cs
@@ -1,23 +1,7 @@
-using System;
-using System.Threading.Tasks;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
-using Microsoft.AspNetCore.Mvc;
-
 namespace BackEnd.Data
 {
     public class AppSettings
     {
         public string Secret { get; set; }
-
-        public async Task<string> GetKey()
-        {
-            string secret = "TBD";
-
-            var secretClient = new SecretClient(new Uri("https://vaultsecretkey.vault.azure.net/"), new DefaultAzureCredential());
-            secret = (await secretClient.GetSecretAsync("Secret")).Value.Value;
-
-            return secret;
-        }
     }
 }
